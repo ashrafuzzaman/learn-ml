c2abf811c8f61083eadf18de90f0e9f5dd83f2b6
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2023-05-31 17:20:44 +0600

    Load data and run column conversion

--- magic-example.py
+++ magic-example.py
@@ -3,3 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import RandomOverSampler
+
+cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
+df = pd.read_csv("data/magic04.data", names=cols)
+# Converting g/h to a boolean for a column
+df["class"] = (df["class"] == "g").astype(int)
+
+print(df)
