5130d0092984960bf1f3e41c3262c05549559ce8
AKM Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2023-06-01 18:19:05 +0600

    Add training data

--- magic-example.py
+++ magic-example.py
@@ -4,12 +4,27 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import RandomOverSampler
 
+def scale_dataset(dataframe, oversample=False):
+  X = dataframe[dataframe.columns[:-1]].values
+  y = dataframe[dataframe.columns[-1]].values
+
+  scaler = StandardScaler()
+  X = scaler.fit_transform(X)
+
+  if oversample:
+    ros = RandomOverSampler()
+    X, y = ros.fit_resample(X, y)
+
+  data = np.hstack((X, np.reshape(y, (-1, 1))))
+
+  return data, X, y
+
+
 cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
 df = pd.read_csv("data/magic04.data", names=cols)
 # Converting g/h to a boolean for a column
 df["class"] = (df["class"] == "g").astype(int)
-
-print(df)
+# print(df)
 
 for label in cols[:-1]:
   plt.hist(df[df["class"]==1][label], color='blue', label='gamma', alpha=0.7, density=True)
@@ -18,4 +33,15 @@ for label in cols[:-1]:
   plt.ylabel("Probability")
   plt.xlabel(label)
   plt.legend()
-  plt.show()
+  # plt.show()
+
+train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
+# print(train, valid, test)
+
+train, X_train, y_train = scale_dataset(train, oversample=True)
+valid, X_valid, y_valid = scale_dataset(valid, oversample=False)
+test, X_test, y_test = scale_dataset(test, oversample=False)
+
+print(train)
+print(X_train)
+print(y_train)
